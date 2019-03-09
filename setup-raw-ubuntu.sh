e55e0f89f3492c2a9357221ed8c3ea3d60e86df8
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2019-03-09 21:18:48 +0200

    Update the raw setup script

--- setup-raw-ubuntu.sh
+++ setup-raw-ubuntu.sh
@@ -1,19 +1,19 @@
 # !/bin/bash
 
 function sudo_setup {
-    sudo apt-get update && \
-    sudo apt-get install software-properties-common -y && \
+    sudo apt update && \
+    sudo apt install software-properties-common -y && \
     sudo apt-add-repository ppa:ansible/ansible -y && \
-    sudo apt-get update && \
-    sudo apt-get install software-properties-common ansible build-essential git -y
+    sudo apt update && \
+    sudo apt install software-properties-common ansible build-essential git -y
 }
 
 function root_setup {
-    apt-get update && \
-    apt-get install software-properties-common -y && \
+    apt update && \
+    apt install software-properties-common -y && \
     apt-add-repository ppa:ansible/ansible -y && \
-    apt-get update && \
-    apt-get install ansible build-essential git -y
+    apt update && \
+    apt install ansible build-essential git -y
 }
 
 (which sudo && sudo_setup) || root_setup
